dad2303f1c84999da974496add4019a2f665ab02
Yusuf <yusuf_zeki1234@hotmail.com>
2024-05-18 14:57:47 +0300

    robot fixed

--- Assets/Scripts/Robots/RobotManager.cs
+++ Assets/Scripts/Robots/RobotManager.cs
@@ -2,89 +2,19 @@ using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class RobotManager : MonoBehaviour
+public class RobotManager : Robot
 {
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _normalSpeed = 3f;
-    [SerializeField] private float _slowSpeed = 1f;
-    Animator _anim;
-    NavMeshAgent _agent;
-    private bool _isSlowed = false;
-    private void Start()
+    protected override void OnClick()
     {
-        _anim = GetComponentInChildren<Animator>();
-        _agent = GetComponent<NavMeshAgent>();
+        //
     }
 
-    private void Update()
+    protected override void OnFollow()
     {
         if (_target is null) return;
 
         _agent.SetDestination(_target.position);
 
-
-        _anim.SetBool("Walk_Anim", Walked());
-    }private void OnEnable()
-    {
-        FirstPersonController.timeControl += SlowMotion;
+        _agent.stoppingDistance = 5;
     }
-
-    private void OnDisable()
-    {
-        FirstPersonController.timeControl -= SlowMotion;
-    }
-
-
-    public void SlowMotion()
-    {
-        _isSlowed = !_isSlowed; 
-        _agent.speed = _isSlowed ? _slowSpeed : _normalSpeed;
-        Debug.Log("11");
-        if (_isSlowed)
-        {
-            Invoke("ResetSpeed", 30f); 
-        }
-    }
-    private void ResetSpeed()
-    {
-        _isSlowed = false;
-        _agent.speed = _normalSpeed;
-    }
-    private void OnEnable()
-    {
-        FirstPersonController.timeControl += SlowMotion;
-    }
-
-    private void OnDisable()
-    {
-        FirstPersonController.timeControl -= SlowMotion;
-    }
-
-
-    public void SlowMotion()
-    {
-        _isSlowed = !_isSlowed;
-        _agent.speed = _isSlowed ? _slowSpeed : _normalSpeed;
-        Debug.Log("11");
-        if (_isSlowed)
-        {
-            Invoke("ResetSpeed", 30f);
-        }
-    }
-    private void ResetSpeed()
-    {
-        _isSlowed = false;
-        _agent.speed = _normalSpeed;
-    }
-
-    #region Walk Value Get
-    private bool Walked()
-    {
-        if (_agent.velocity.magnitude > 0)
-        {
-            return true;
-        }
-        return false;
-    }
-    #endregion
 }
